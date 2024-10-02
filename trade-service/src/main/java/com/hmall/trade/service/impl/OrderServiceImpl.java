ffeb156c0fcccc50a090faa0031e7dbcb19846cf
ZenSyntax <moreantx@gmail.com>
2024-10-02 11:11:30 +0800

    完成了延迟消息业务改造

--- trade-service/src/main/java/com/hmall/trade/service/impl/OrderServiceImpl.java
+++ trade-service/src/main/java/com/hmall/trade/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmall.trade.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmall.api.client.CartClient;
 import com.hmall.api.client.ItemClient;
@@ -7,14 +8,17 @@ import com.hmall.api.dto.ItemDTO;
 import com.hmall.api.dto.OrderDetailDTO;
 import com.hmall.common.exception.BadRequestException;
 import com.hmall.common.utils.UserContext;
+import com.hmall.trade.constants.MQConstants;
 import com.hmall.trade.domain.dto.OrderFormDTO;
 import com.hmall.trade.domain.po.Order;
 import com.hmall.trade.domain.po.OrderDetail;
+import com.hmall.trade.mapper.OrderDetailMapper;
 import com.hmall.trade.mapper.OrderMapper;
 import com.hmall.trade.service.IOrderDetailService;
 import com.hmall.trade.service.IOrderService;
 import io.seata.spring.annotation.GlobalTransactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -42,6 +46,9 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     private final CartClient cartClient;
 
     private final ItemClient itemClient;
+
+    private final RabbitTemplate rabbitTemplate;
+    private final OrderDetailMapper orderDetailMapper;
 
     @Override
     @GlobalTransactional
@@ -86,6 +93,17 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
         } catch (Exception e) {
             throw new RuntimeException("库存不足！");
         }
+
+        //5.发送延迟消息，检查订单状态
+        rabbitTemplate.convertAndSend(
+                MQConstants.DELAY_EXCHANGE_NAME,
+                MQConstants.DELAY_ORDER_KEY,
+                order.getId(),
+                message->{
+                    message.getMessageProperties().setDelay(10000);
+                    return message;
+                }
+        );
         return order.getId();
     }
 
@@ -97,6 +115,43 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
         order.setStatus(2);
         order.setPayTime(LocalDateTime.now());
         updateById(order);
+    }
+
+    @Override
+    public void cancelOrder(Long orderId) {
+        //1.标记订单已关闭
+        Order order = getById(orderId);
+
+        //1.1幂等性判断
+        if (order != null && order.getStatus() == 5) {
+            //此处意味着订单已经被修改过了，不需要再进行修改
+            return;
+        }
+
+        order.setStatus(5);
+        updateById(order);
+
+        //2.恢复库存
+        List<OrderDetail> orderDetails = orderDetailMapper.selectList(new LambdaQueryWrapper<OrderDetail>()
+                .eq(OrderDetail::getOrderId, orderId));
+        // 使用 Stream API 获取所有 itemId 并封装到 List<Long> 中
+        List<Long> itemIds = orderDetails.stream()
+                .map(OrderDetail::getItemId)  // 提取 itemId
+                .collect(Collectors.toList());  // 收集到 List<Long> 中
+        List<ItemDTO> itemDTOS = itemClient.queryItemByIds(itemIds);
+        for (Long itemId : itemIds) {
+            //取出该商品被扣减的数量
+            ItemDTO matchedItem = itemDTOS.stream()
+                    .filter(item -> item.getId().equals(itemId))
+                    .findFirst()
+                    .orElse(null);
+            OrderDetail orderDetail = orderDetails.stream()
+                    .filter(orderDetail1 -> orderDetail1.getItemId().equals(itemId))
+                    .findFirst()
+                    .orElse(null);
+            matchedItem.setStock(matchedItem.getStock() + orderDetail.getNum());
+            itemClient.recoverItem(matchedItem);
+        }
     }
 
     private List<OrderDetail> buildDetails(Long orderId, List<ItemDTO> items, Map<Long, Integer> numMap) {
